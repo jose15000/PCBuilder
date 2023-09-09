47aedfc09666cec90955466b07d5de09031f23c5
José Henrique Oliveira de Carvalho <98109872+jose15000@users.noreply.github.com>
2023-09-09 02:32:17 -0300

    script que recupera os dados do JSON

--- script.js
+++ script.js
@@ -1,20 +1,14 @@
+
 var myHeaders = new Headers();
 myHeaders.append("X-API-KEY", "2f8681d3ebd7535a7df8fa621c869bcb1bc07009");
 myHeaders.append("Content-Type", "application/json");
 
-var processador = document.getElementById("cpu");
-
-
-processador.addEventListener("click", e =>{
-    e.preventDefault();
-
-    var raw = JSON.stringify({
+var raw = JSON.stringify({
         "q": "https://www.amazon.com.br/s?k=processador+intel",
         "gl": "br",
         "num": 30,
         "hl": "en"
       });
-      
       var requestOptions = {
         method: 'POST',
         headers: myHeaders,
@@ -22,8 +16,35 @@ processador.addEventListener("click", e =>{
         redirect: 'follow'
       };
       fetch("https://google.serper.dev/search", requestOptions)
-        .then(response => response.text())
-        .then(result => JSON.parse(result))
-        .then(result => console.log(result))
+        .then(response => response.json())
+        .then(data =>{
+          function preenchetabela(data){
+            const tabela = document.getElementById('tabela-cpu');
+            const tbody = tabela.querySelector("#tbody");
+            const regex = /^\d{3,}$|^\d+\.\d{3}$/;
+            data.forEach(element => {
+              const row = document.createElement('tr');
+              const titleCell = document.createElement('td');
+              titleCell.textContent = element.title;
+              const pricingCell = document.createElement('td');
+              pricingCell.textContent = element.price;
+                if(element.price && regex.test(element.price.toString()))
+                {
+                  row.appendChild(titleCell);
+                  row.appendChild(pricingCell);
+                  tbody.appendChild(row);
+                }
+
+            });
+            
+
+          }
+          preenchetabela(data.organic);
+        })
+        
+        
         .catch(error => console.log('error', error));
-})
+
+        
+        
+
